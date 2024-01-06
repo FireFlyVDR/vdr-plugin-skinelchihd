50457573545fe5b1e9f937b756791f34492565e7
FireFly <christoph1.haubrich@arcor.de>
2024-01-06 14:35:41 +0100

    Release 1.2.4

--- skinelchihd.c
+++ skinelchihd.c
@@ -53,7 +53,7 @@
 #error THIS VERSION OF THE SKINELCHIHD-PLUGIN REQUIRES AT LEAST VDR 2.4.3
 #endif
 
-static const char *VERSION      = "1.2.3" ;
+static const char *VERSION      = "1.2.4" ;
 static const char *DESCRIPTION  = trNOOP("ElchiHD skin");
 const char *OSDSKIN             = "ElchiHD";
 
